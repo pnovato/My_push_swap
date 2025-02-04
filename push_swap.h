e7d0779cd089e558c370e3714a934ad71c4e8ffa
pnovato <ptrck.nvt.slv@gmail.com>
2025-02-04 16:37:25 +0000

    adding some more funtions into movements.c

--- push_swap.h
+++ push_swap.h
@@ -12,5 +12,18 @@ typedef struct t_list
 }s_list
 
 
+//movements logic
+void    swap(s_list **a);
+int     push(s_list **stack_destiny, s_list **stack_origin);
+
+//swap movement
+int     sa(s_list **stack_a);
+int     sb(s_list **stack_b);
+int     ss(s_list **stack_a, s_list **stack_b);
+
+//push movement
+int     pa(s_list **stack_a, s_list **stack_b);
+int     pb(s_list **stack_b, s_list **stack_a);
+
 #endif
 
