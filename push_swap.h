1673f41c9de1783a8df85f2c7fcf97676bd267d3
pnovato <ptrck.nvt.slv@gmail.com>
2025-02-11 13:20:40 +0000

    adding functions to push_swap.h

--- push_swap.h
+++ push_swap.h
@@ -1,20 +1,22 @@
-#ifndef
+#ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
 # include <unistd.h>
-# include 
+# include <stdlib.h>
 
 typedef struct t_list
 {
 	int value;
 	int index;
-	s_stack *next;
-}s_list
+	struct t_list *next;
+}s_list;
 
 
 //movements logic
 void    swap(s_list **a);
 int     push(s_list **stack_destiny, s_list **stack_origin);
+int     rotate(s_list **stack_master);
+int     reverseRotate(s_list **stack_master);
 
 //swap movement
 int     sa(s_list **stack_a);
@@ -24,6 +26,16 @@ int     ss(s_list **stack_a, s_list **stack_b);
 //push movement
 int     pa(s_list **stack_a, s_list **stack_b);
 int     pb(s_list **stack_b, s_list **stack_a);
+
+//rotate movement
+int     ra(s_list **stack_a);
+int     rb(s_list **stack_b);
+int     rr(s_list **stack_a, s_list **stack_b);
+
+//reverseRotate movement
+int     rra(s_list **stack_a);
+int     rrb(s_list **stack_b);
+int     rrr(s_list **stack_a, s_list **stack_b);
 
 #endif
 
